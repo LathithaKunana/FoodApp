7a9350a198960aa5209ab7fde7312a82626d509c
LathithaKunana <lathitha.kunana@younglings.africa>
2024-03-18 21:17:53 +0200

    still busy with the welcome screen/splashscreen

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar, Image } from 'react-native'
 import React from 'react'
 
 export default function WelcomeScreen() {
   return (
-    <View>
-      <Text>WelcomeScreen</Text>
+    <View className = "flex-1 justify-center items-center space-y-10 bg-amber-500">
+      <StatusBar barStyle={'light-content'}/>
+
+      {/* logo image with rings */}
+      <View className="bg-white/20 rounded-full p-10">
+        <View className="bg-white/20 rounded-full p-8">
+            <Image source={require('../assets/images/foodlogo.png')} 
+                style={{width: 200, height: 200}}
+            />
+
+        </View>
+      </View>
     </View>
   )
 }
