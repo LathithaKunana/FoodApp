0c4d26413a8fa21f88b83c24d0361cb107a01db1
LathithaKunana <lathitha.kunana@younglings.africa>
2024-03-18 20:34:00 +0200

    started a with new app named FoodApp

--- App.js
+++ App.js
@@ -1,20 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import AppRoute from './appRoute/AppRoute';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <AppRoute/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ appRoute/AppRoute.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import WelcomeScreen from '../screens/WelcomeScreen'
+import HomeScreen from '../screens/HomeScreen'
+const Stack = createNativeStackNavigator();
+
+export default function AppRoute() {
+  return (
+   <NavigationContainer>
+    <Stack.Navigator initialRouteName='Welcome' screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Welcome' component={WelcomeScreen}/>
+        <Stack.Screen name='Home' component={HomeScreen}/>
+    </Stack.Navigator>
+   </NavigationContainer>
+  ) 
+}
